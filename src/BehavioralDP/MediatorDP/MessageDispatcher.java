1107e74f191ca8f47ee56633c40082d0160ac6c1
İlker Şelimen <ilkerselimen@gmail.com>
2023-05-09 22:23:39 +0300

    Mediator

---
+++ src/BehavioralDP/MediatorDP/MessageDispatcher.java
@@ -0,0 +1,28 @@
+package BehavioralDP.MediatorDP;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MessageDispatcher implements Dispatcher {
+
+    // butun actorleri uzman oldugu topic ile setlemem lazim
+    Map<String, Actor> registeredActors = new HashMap<>();
+
+    // actorleri kaydeden method
+    void register(String topic,Actor actor){
+        registeredActors.put(topic,actor);
+    }
+
+    @Override
+    public void dispatch(String topic, String message) {
+        // topic ile ilgili uzmani cagiriyorum
+        Actor actor = registeredActors.get(topic);
+
+        if (actor==null){
+            System.out.println("No actor registered for this topic");
+        } else {
+            actor.receiveMessage(message);
+        }
+
+    }
+}
